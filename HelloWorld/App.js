8be1d38cda257f3e8edc58203d5b24468f56196e
Hari Krishna <iamharikrishnaa@gmail.com>
2023-07-25 16:46:42 +0530

    first commit

---
+++ HelloWorld/App.js
@@ -0,0 +1,7 @@
+const head =React.createElement(
+    "h3",
+    {id:"heading"},
+    "helloReactfromApp"
+    )
+const rooting = ReactDOM.createRoot(document.getElementById("root2"));
+rooting.render(head);
